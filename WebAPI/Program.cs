8c0c2e538121bea690862a6c82a8f29324d3e2fd
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-08 00:35:07 +0300

    Add data access service

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,9 +1,11 @@
+using DataAccess;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-
+builder.Services.AddDataAccessServices(builder.Configuration);
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
