240a4785339164eaba561f77fc513633efa10ec7
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-08 00:34:16 +0300

    Create service registration

---
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using DataAccess.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataAccess;
+
+public static class DataAccessServiceRegistration
+{
+    public static void AddDataAccessServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<SilversoftContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        });
+    }
+}
