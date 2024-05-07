2a3d7d7296784e233d314704bc583e9dd4b18a45
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-08 00:34:06 +0300

    Create db context

---
+++ DataAccess/Contexts/SilversoftContext.cs
@@ -0,0 +1,19 @@
+﻿using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataAccess.Contexts;
+
+public class SilversoftContext(DbContextOptions<SilversoftContext> options) : DbContext(options)
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Challenge> Challenges { get; set; }
+    public DbSet<Event> Events { get; set; }
+    public DbSet<Hint> Hints { get; set; }
+    public DbSet<Prize> Prizes { get; set; }
+    public DbSet<Question> Questions { get; set; }
+    public DbSet<QuestionSolver> QuestionSolvers { get; set; }
+    public DbSet<Role> Roles { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserPrize> UserPrizes { get; set; }
+}
