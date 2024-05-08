0fb5d1040abec49b1a630eb5e29f50fbc314e0c4
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-09 00:26:54 +0300

    Create role repository

---
+++ DataAccess/Concretes/EntityFramework/EfRoleRepository.cs
@@ -0,0 +1,7 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Contexts;
+using Entities.Concretes;
+
+namespace DataAccess.Concretes.EntityFramework;
+
+public class EfRoleRepository : EfEntityRepository<Role,SilversoftContext> ,IRoleRepository;
