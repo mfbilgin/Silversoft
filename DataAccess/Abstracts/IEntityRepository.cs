a4ca6107cc0933bb6f92f0b0e136eea9ee519063
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-08 23:43:28 +0300

    Create generic repository

---
+++ DataAccess/Abstracts/IEntityRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Core.Extensions.Paging;
+using Entities.Abstracts;
+
+namespace DataAccess.Abstracts;
+
+public interface IEntityRepository<T> where T : class, IEntity, new()
+{
+    void Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    T? Get(Expression<Func<T, bool>> filter);
+    PageableModel<T> GetList(Expression<Func<T, bool>>? filter = null, int index = 1, int size = 10);
+}
---
+++ DataAccess/Concretes/EntityFramework/EfEntityRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Linq.Expressions;
+using Core.Extensions.Paging;
+using DataAccess.Abstracts;
+using Entities.Abstracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes.EntityFramework;
+
+public class EfEntityRepository<TEntity, TContext> : IEntityRepository<TEntity>
+    where TEntity : class, IEntity, new()
+    where TContext : DbContext, new()
+{
+    public void Add(TEntity entity)
+    {
+        using var context = new TContext();
+        var addedEntity = context.Entry(entity);
+        addedEntity.State = EntityState.Added;
+        context.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        using var context = new TContext();
+        var updatedEntity = context.Entry(entity);
+        updatedEntity.State = EntityState.Modified;
+        context.SaveChanges();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        using var context = new TContext();
+        var deletedEntity = context.Entry(entity);
+        deletedEntity.State = EntityState.Deleted;
+        context.SaveChanges();
+    }
+
+    public TEntity? Get(Expression<Func<TEntity, bool>> filter)
+    {
+        using var context = new TContext();
+        return context.Set<TEntity>().SingleOrDefault(filter);
+    }
+
+    public PageableModel<TEntity> GetList(Expression<Func<TEntity, bool>>? filter = null,int index=1, int size=10)
+    {
+        using var context = new TContext();
+        var queryable = filter is null
+            ? context.Set<TEntity>()
+            : context.Set<TEntity>().Where(filter);
+        return queryable.ToPaginate(index, size);
+    }
+}
