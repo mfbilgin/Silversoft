aa5a8fc0a42ade8594a51ff67867c9e97a61b9d0
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-09 00:26:25 +0300

    Create generic entity service

---
+++ Business/Abstracts/IEntityService.cs
@@ -0,0 +1,12 @@
+﻿using Core.Extensions.Paging;
+
+namespace Business.Abstracts;
+
+public interface IEntityService<T>
+{
+    public void Add(T entity);
+    public void Update(T entity);
+    public void Delete(T entity);
+    public T? GetById(int id);
+    public PageableModel<T> GetAll();
+}
