520116eb03dba664d20919b606290b9a8c08f171
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-12 13:37:54 +0300

    Create security aspect

---
+++ Core/Aspects/Autofac/Security/SecurityAspect.cs
@@ -0,0 +1,33 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.Extensions.Claim;
+using Core.Interceptors;
+using Core.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.Aspects.Autofac.Security;
+
+public class SecurityAspect(string roles) : MethodInterception
+{
+    private readonly string[] _roles = roles.Split(',');
+
+    private readonly IHttpContextAccessor? _httpContextAccessor =
+        ServiceTool.ServiceProvider?.GetService<IHttpContextAccessor>();
+
+    protected override void OnBefore(IInvocation invocation)
+    {
+        if (_httpContextAccessor == null)
+        {
+            throw new NullReferenceException("HttpContextAccessor is null.");
+        }
+
+        var roleClaims = _httpContextAccessor.HttpContext.User.ClaimRoles();
+        if (_roles.Any(role => roleClaims.Contains(role)))
+        {
+            return;
+        }
+
+        throw new AuthorizationException();
+    }
+}
