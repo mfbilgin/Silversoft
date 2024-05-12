93da835cfb4cd614e1e995f1d42dad187917191c
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-12 13:59:29 +0300

    Create claim extensions

---
+++ Core/Extensions/Claim/ClaimExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Core.Extensions.Claim;
+
+public static class ClaimExtensions
+{
+    public static void AddEmail(this ICollection<System.Security.Claims.Claim> claims, string email)
+    {
+        claims.Add(new System.Security.Claims.Claim(JwtRegisteredClaimNames.Email, email));
+    }
+
+    public static void AddName(this ICollection<System.Security.Claims.Claim> claims, string name)
+    {
+        claims.Add(new System.Security.Claims.Claim(ClaimTypes.Name, name));
+    }
+
+    public static void AddNameIdentifier(this ICollection<System.Security.Claims.Claim> claims, string nameIdentifier)
+    {
+        claims.Add(new System.Security.Claims.Claim(ClaimTypes.NameIdentifier, nameIdentifier));
+    }
+
+    public static void AddRoles(this ICollection<System.Security.Claims.Claim> claims, string[] roles)
+    {
+        roles.ToList().ForEach(role => claims.Add(new System.Security.Claims.Claim(ClaimTypes.Role, role)));
+    }
+}
