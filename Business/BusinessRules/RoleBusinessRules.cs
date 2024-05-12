20563787fe71872479c4f394b2a0cded6b096f24
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-12 14:04:06 +0300

    Create role business rules

---
+++ Business/BusinessRules/RoleBusinessRules.cs
@@ -0,0 +1,24 @@
+﻿using Business.Constants;
+using DataAccess.Abstracts;
+
+namespace Business.BusinessRules;
+
+public class RoleBusinessRules(IRoleRepository roleRepository)
+{
+    public void RoleNameCanNotBeDuplicated(string name)
+    {
+        var role = roleRepository.GetByName(name);
+        if (role is not null)
+        {
+            throw new Exception(RoleMessages.RoleNameAlreadyExists);
+        }
+    }
+    public void RoleIdCanBeExist(Guid id)
+    {
+        var role = roleRepository.GetById(id);
+        if (role is null)
+        {
+            throw new Exception(RoleMessages.RoleNotFound);
+        }
+    }
+}
