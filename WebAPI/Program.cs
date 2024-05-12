366e7da58b7899c71075a659bb5e73092c34d5fd
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-12 14:03:25 +0300

    Add IoC,Business and core services

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,19 +1,35 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business;
+using Business.DependencyResolvers;
+using Core;
+using Core.CrossCuttingConcerns.Exceptions;
+using Core.DependencyResolvers;
 using DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()); 
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+    {
+        containerBuilder.RegisterModule(new AutofacBusinessModule());
+    });
+builder.Services.AddAuthentication();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
-builder.Services.AddDataAccessServices(builder.Configuration);
+builder.Services.AddDataAccessServices();
+builder.Services.AddBusinessServices();
+builder.Services.AddCoreServices(new CoreModule());
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.ConfigureCustomExceptionMiddleware();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.UseAuthentication();
 app.MapControllers();
+app.Run();
