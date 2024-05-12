7fa6f3fcccf74475a4a00db0ee1b3cbf8f0e64d3
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-05-12 14:04:26 +0300

    Create role messages

---
+++ Business/Constants/RoleMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace Business.Constants;
+
+public static class RoleMessages
+{
+    public const string RoleNameAlreadyExists = "Role name already exists.";
+    public const string RoleNotFound = "Role not found.";
+}
