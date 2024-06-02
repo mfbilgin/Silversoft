181e72aaca9a47697a57095c3fe5b886c42a49d2
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-03 01:35:07 +0300

    Create user controller without functions

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,13 @@
+﻿using Business.Abstracts;
+using Dtos.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(IUserService userService) : ControllerBase
+{
+    
+}
