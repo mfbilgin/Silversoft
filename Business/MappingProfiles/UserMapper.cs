95b30bf5775b9aeca4bd90358060261fbf998dbd
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-03 01:34:39 +0300

    Create user mapper

---
+++ Business/MappingProfiles/UserMapper.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core.Extensions.Paging;
+using Dtos.User;
+using Entities.Concretes;
+
+namespace Business.MappingProfiles;
+
+public sealed class UserMapper : Profile
+{
+    public UserMapper()
+    {
+        CreateMap<User, UserUpdateDto>().ReverseMap();
+        CreateMap<User, UserDeleteDto>().ReverseMap();
+        CreateMap<User, UserGetDto>().ReverseMap();
+        CreateMap<PageableModel<User>, PageableModel<UserGetDto>>().ReverseMap();
+    }
+}
