756882bdaa8e6aec8d095e83ffa80cb2a4fdcacd
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-02 14:10:02 +0300

    Create error details

---
+++ Core/Exceptions/Details/AuthorizationErrorDetails.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace Core.Exceptions.Details;
+
+public class AuthorizationErrorDetails : IErrorDetails
+{
+    public int StatusCode { get; } = StatusCodes.Status401Unauthorized;
+    public string Message { get; } = "You are not authorized to access this resource.";
+    public string GetDetails()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
