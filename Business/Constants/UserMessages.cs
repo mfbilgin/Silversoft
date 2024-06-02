4886331b76f0dc1f4bdd18b405f527373c165171
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-03 01:34:51 +0300

    Create user messages

---
+++ Business/Constants/UserMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace Business.Constants;
+
+public static class UserMessages
+{
+    public const string UsernameAlreadyExists = "Username already exists.";
+    public const string UserNotFound = "User not found.";
+    public const string CurrentPasswordIsIncorrect = "Current password is incorrect.";
+    public const string NewPasswordMustBeDifferent = "New password must be different from the current password.";
+}
