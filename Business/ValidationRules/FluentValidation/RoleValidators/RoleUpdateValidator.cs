ff7d03b0db62b3697eb0266c9bc5dabfd82cb5ce
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-02 14:21:21 +0300

    Create RoleUpdateValidator.cs

---
+++ Business/ValidationRules/FluentValidation/RoleValidators/RoleUpdateValidator.cs
@@ -0,0 +1,15 @@
+﻿using Dtos.Role;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation.RoleValidators;
+
+public class RoleUpdateValidator : AbstractValidator<RoleUpdateDto>
+{
+    public RoleUpdateValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Role name cannot be empty.");
+        RuleFor(x => x.Name).NotNull().WithMessage("Role name cannot be null.");
+        RuleFor(x => x.Name).MinimumLength(3).WithMessage("Role name must be at least 3 characters.");
+        RuleFor(x => x.Name).MaximumLength(50).WithMessage("Role name must be at most 50 characters.");
+    }
+}
