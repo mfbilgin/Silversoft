44b79f648c0ec3a84fdde5595ad5581b38c21107
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-02 14:21:05 +0300

    Create Rolemapper

---
+++ Business/MappingProfiles/RoleMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Core.Extensions.Paging;
+using Dtos.Role;
+using Entities.Concretes;
+
+namespace Business.MappingProfiles;
+
+public class RoleMapper : Profile
+{
+    public RoleMapper()
+    {
+        CreateMap<Role, RoleAddDto>().ReverseMap();
+        CreateMap<Role, RoleUpdateDto>().ReverseMap();
+        CreateMap<Role, RoleDeleteDto>().ReverseMap();
+        CreateMap<Role, RoleGetDto>().ReverseMap();
+        CreateMap<PageableModel<Role>, PageableModel<RoleGetDto>>().ReverseMap();
+    }
+}
