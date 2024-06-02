91cfc73ad92e0c063b93fb6abe261f999d03b4f5
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-02 14:21:44 +0300

    Create UserService

---
+++ Business/Abstracts/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using Core.Extensions.Paging;
+using Dtos.Role;
+using Dtos.User;
+
+namespace Business.Abstracts;
+
+public interface IUserService
+{
+    public void Add(UserAddDto userAddDto);
+    public void Update(UserUpdateDto userUpdateDto);
+    public void Delete(UserDeleteDto userDeleteDto);
+    public UserGetDto? GetById(Guid id);
+    public PageableModel<UserGetDto> GetAll(int index = 1, int size = 10);
+    UserGetDto? GetByUsername(string name);
+}
