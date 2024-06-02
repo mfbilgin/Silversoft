f3f50525d15d71d06d03266f1ce1a5dff440ae29
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-03 01:33:32 +0300

    Create user validators

---
+++ Business/ValidationRules/FluentValidation/UserValidators/UserAddValidator.cs
@@ -0,0 +1,42 @@
+﻿using Dtos.User;
+using Entities.Concretes;
+using FluentValidation;
+
+namespace Business.ValidationRules.FluentValidation.UserValidators;
+
+public class UserAddValidator : AbstractValidator<User>
+{
+    public UserAddValidator()
+    {
+        RuleFor(user => user.Username).NotEmpty().WithMessage("Username cannot be empty.");
+        RuleFor(user => user.Username).NotNull().WithMessage("Username cannot be null.");
+        RuleFor(user => user.Username).MinimumLength(3).WithMessage("Username must be at least 3 characters.");
+        RuleFor(user => user.Username).MaximumLength(50).WithMessage("Username must be at most 50 characters.");
+        
+        RuleFor(user => user.FirstName).NotEmpty().WithMessage("Username cannot be empty.");
+        RuleFor(user => user.FirstName).NotNull().WithMessage("Username cannot be null.");
+        RuleFor(user => user.FirstName).MinimumLength(3).WithMessage("Username must be at least 3 characters.");
+        RuleFor(user => user.FirstName).MaximumLength(50).WithMessage("Username must be at most 50 characters.");
+        
+        RuleFor(user => user.LastName).NotEmpty().WithMessage("Username cannot be empty.");
+        RuleFor(user => user.LastName).NotNull().WithMessage("Username cannot be null.");
+        RuleFor(user => user.LastName).MinimumLength(3).WithMessage("Username must be at least 3 characters.");
+        RuleFor(x => x.LastName).MaximumLength(50).WithMessage("Username must be at most 50 characters.");
+        
+        RuleFor(user => user.RoleId).NotEmpty().WithMessage("Role cannot be empty.");
+        RuleFor(user => user.RoleId).NotNull().WithMessage("Role cannot be null.");
+        
+        RuleFor(user => user.StudentNumber).Must(IsStudentNumberValid).WithMessage("Student number must be 10 characters.");
+
+        bool IsStudentNumberValid(string? studentNumber)
+        {
+            if (string.IsNullOrWhiteSpace(studentNumber))
+            {
+                return true;
+            }
+
+            return studentNumber.Length == 10;
+        }
+        
+    }
+}
