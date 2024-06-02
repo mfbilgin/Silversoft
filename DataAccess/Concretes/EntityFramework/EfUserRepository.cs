e882fcbd581bc630a6293ab2f0e24f2f15528c15
Muhammet BİLGİN <mf.bilgin0@hotmail.com>
2024-06-02 14:43:40 +0300

    Create user repository

---
+++ DataAccess/Concretes/EntityFramework/EfUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccess.Abstracts;
+using DataAccess.Abstracts.EntityFramework;
+using Entities.Concretes;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concretes.EntityFramework;
+
+public class EfUserRepository(DbContext context) : EfEntityRepository<User>(context), IUserRepository
+{
+    
+}
